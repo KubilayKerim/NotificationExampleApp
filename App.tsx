75159a28dd297857cabe9320c18d3fbcd3576304
Kubilay Kerim Özdemir <kubilay.ozdemir@net-bt.com.tr>
2022-04-19 04:41:07 +0300

    recieving data from notification

--- App.tsx
+++ App.tsx
@@ -8,8 +8,9 @@
  * @format
  */
 
-import React from 'react';
-import {
+ import React, { useEffect } from 'react';
+ import {
+   Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -18,12 +19,92 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import notifee, { EventType } from '@notifee/react-native'
+import { loadOptions } from '@babel/core';
 
 const App = () => {
+
+  const [recieveddata, setRecieveddata] = React.useState('');
+
+  useEffect(() => {
+    return notifee.onForegroundEvent(async ({ type, detail }) => {
+      if (type === EventType.ACTION_PRESS) {
+        setRecieveddata(detail.pressAction?.id as string);
+      }
+    });
+  });
+
+  async function DisplayNotification(remoteMessage) {
+    // Create a channel
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: remoteMessage.notification.title,
+      body: remoteMessage.notification.body,
+      data: remoteMessage.data,
+      android: {
+        channelId,
+        smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
+        actions: [
+          {
+            title: remoteMessage.data.choice1,
+            pressAction: { id: remoteMessage.data.choice1 },
+          },
+          {
+            title: remoteMessage.data.choice2,
+            pressAction: { id: remoteMessage.data.choice2 },
+          },
+        ],
+      },
+    });
+  }
+  const requestUserPermission = async () => {    
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      getFcmToken() //<---- Add this
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getFcmToken = async () => {
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+     console.log(fcmToken);
+     console.log("Your Firebase Token is:", fcmToken);
+    } else {
+     console.log("Failed", "No token received");
+    }
+  }
+
+
+  useEffect(() => {
+    requestUserPermission();
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      DisplayNotification(remoteMessage);
+
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <SafeAreaView >
       <Text>NotificationExampleApp</Text>
+      <View>
+      <Text>{recieveddata}</Text>
+    </View>
     </SafeAreaView>
   );
 };
--- index.js
+++ index.js
@@ -5,5 +5,26 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+import notifee, { EventType } from '@notifee/react-native'
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+  });
+
+  notifee.onBackgroundEvent(async ({type, detail}) => {
+      console.log("notifee on background")
+    console.log("Background Event: ", type, detail);
+    
+    const {notification, pressAction} = detail;
+
+    if (type === EventType.PRESS) {
+        console.log('User pressed an action with the id: ', pressAction.id);
+        // navigate here
+        // return pressAction.id;
+    }
+    await notifee.cancelNotification(notification.id);
+    console.log('background-event');
+});
 
 AppRegistry.registerComponent(appName, () => App);
